b19ec161997b1fd95dc648ae38e46ce729dcf826
prakhar-s-srivastava <prakhar.s.srivastava@gmail.com>
2021-12-15 02:34:46 +0530

    inital commit

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import Canvas from "./Canvas";
+import "../Css/Neomorphics.css"
+import "../Css/Home.css"
+
+function Home() {
+ 
+
+  return (
+    <div className="App">
+      <Canvas />
+          <div className="neomorphics-up">
+              <img src="pic_trulli.jpg" alt="Italian Trulli"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
