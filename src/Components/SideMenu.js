5f6c4b8286401cec2463eee42ec3f31e43848ce9
Prakhar <prakhar.s.srivastava@gmail.com>
2022-12-13 18:10:28 +0530

    added links

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -10,17 +10,17 @@ export default function  ()
     return (
       <div className=" sideMenu">
         <div className="neomorphics-round">
-          <a >
+          <a href="https://www.instagram.com/flux.blue_only/" target="blank">
             <img className="SidemenuImage eff1  eff2" src={instagram}></img>
           </a>
         </div>
         <div className="neomorphics-round">
-          <a>
+          <a href="https://www.facebook.com/prakhars.srivastava.7/" target="blank">
             <img className="SidemenuImage eff1  eff2" src={facebook}></img>
           </a>
         </div>
         <div className="neomorphics-round">
-          <a>
+          <a href="www.linkedin.com/in/prakharsrivastavagamedeveloper" target="blank">
             <img className="SidemenuImage eff1  eff2" src={linkedin}></img>
           </a>
         </div>
